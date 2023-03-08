956a85f3bbce0980a74635860aab958518a84d96
Joel Marriott <joel.marriott.uk@gmail.com>
2023-03-08 06:55:04 +0000

    Docstrings

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from r_window import Window
 
 
 def main():
+    "Main game loop"
     window = Window()
     clock = pygame.time.Clock()
     
@@ -15,6 +16,15 @@ def main():
     
 
 def input(events, window):
+    """Handles all game input
+
+    Args:
+        events (pygame.event): All events captured by pygame
+        window (r_window.Window): The window instance of the rogue engine
+
+    Returns:
+        Boolean: Keep game running
+    """
     run = True
     for event in events:
         if event.type == pygame.QUIT:
--- r_window.py
+++ r_window.py
@@ -8,11 +8,20 @@ class Window:
         self.display = pygame.display.set_mode((self.width, self.height))
         self.fullscreen = False
         
-    def draw(self):
+    def draw(self, objects=[]):
+        """Draws everything to the display
+
+        Args:
+            objects (list, optional): Object with image, position and
+            collision data. Must have a draw method. Defaults to [].
+        """
         self.display.fill(pygame.Color(79,164,184))
+        for object in objects:
+            objects[object].draw()
         pygame.display.update()
         
     def toggle_fullscreen(self):
+        "Toggles between fullscreen mode and small window"
         if self.fullscreen:
             self.display = pygame.display.set_mode((self.width, self.height))
             self.fullscreen = False
